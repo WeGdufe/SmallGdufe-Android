bf8024f11afb4f0999658ec28b647adaab176c81
xiaoguang <wintercoder@gmail.com>
2017-05-30 16:54:51 +0800

    新增平时成绩的显示，去掉关于页的商务邮箱

--- app/src/main/java/com/guang/app/adapter/ScoreAdapter.java
+++ app/src/main/java/com/guang/app/adapter/ScoreAdapter.java
@@ -1,6 +1,8 @@
 package com.guang.app.adapter;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.CompoundButton;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -26,20 +28,53 @@ public class ScoreAdapter extends BaseQuickAdapter<Score, BaseViewHolder> {
                     .setText(R.id.tv_score,""+item.getScore())
                     .setText(R.id.tv_credit,""+item.getCredit())
                     .setText(R.id.tv_grade_point, CalcUtils.calcScore2Gpa(item.getScore()))
-                    .setVisible(R.id.expandable, false);
-        //  .setVisible(R.id.expandable, viewHolder.getView(R.id.expandable_toggle_button).is())
-        //重修判断 60分为通过？
-        int score = item.getScore();
-        if( score < 60) {
-            viewHolder.setTextColor(R.id.tv_course_name, mContext.getResources().getColor(R.color.goal_item_failed_color));
-            viewHolder.setTextColor(R.id.tv_score, mContext.getResources().getColor(R.color.goal_item_failed_color));
-        }else{
-            viewHolder.setTextColor(R.id.tv_score, mContext.getResources().getColor(R.color.goal_item_passed_color));
-        }
+                    .setVisible(R.id.score_expandable, false)
+                    .setVisible(R.id.score_item_separator, false);
 
+        //下拉后的内容
+        viewHolder.setText(R.id.tv_classcode,""+item.getClassCode())
+                .setText(R.id.tv_score_exp,""+item.getExpScore())
+                .setText(R.id.tv_score_daily,""+item.getDailyScore())
+                .setText(R.id.tv_score_paper,""+item.getPaperScore());
+
+        //点击下拉按钮显示/隐藏额外信息，BaseRecyclerViewAdapterHelper框架也能实现，这里手动实现 https://github.com/CymChad/BaseRecyclerViewAdapterHelper/wiki/%E5%88%86%E7%BB%84%E7%9A%84%E4%BC%B8%E7%BC%A9%E6%A0%8F
+        viewHolder.setOnCheckedChangeListener(R.id.score_btn_expandable_toggle, new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                viewHolder.setVisible(R.id.score_expandable, isChecked);
+                viewHolder.setVisible(R.id.score_item_separator, isChecked);
+            }
+        });
+        //让整个内容条的点击都生效 显示/隐藏信息 ，提高体验
+        viewHolder.getView(R.id.layout_score_item_content).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId() != R.id.score_btn_expandable_toggle) {  //虽然想防止多次点击（听声音） 但好像没什么效果，该条件要不要都行
+                    viewHolder.getView(R.id.score_btn_expandable_toggle).performClick();
+                }
+            }
+        });
+
+        colorTextViewByScore(viewHolder,R.id.tv_score,item.getScore());
+        colorTextViewByScore(viewHolder,R.id.tv_score_daily,item.getDailyScore());
+        colorTextViewByScore(viewHolder,R.id.tv_score_paper,item.getPaperScore());
     }
+
+    //根据分数填充颜色，实验成绩不需要填充，暂没做 重修判断
+    private void colorTextViewByScore(BaseViewHolder viewHolder,int textViewId, int score) {
+        if( score < 60) {
+            viewHolder.setTextColor(textViewId, mContext.getResources().getColor(R.color.goal_item_failed_color));
+            if(textViewId == R.id.tv_score){
+                viewHolder.setTextColor(R.id.tv_course_name, mContext.getResources().getColor(R.color.goal_item_failed_color));
+            }
+        }else{
+            viewHolder.setTextColor(textViewId, mContext.getResources().getColor(R.color.goal_item_passed_color));
+        }
+    }
+
     public void cleanData(){
         super.mData.clear();
     }
+
 
 }
--- app/src/main/java/com/guang/app/model/Score.java
+++ app/src/main/java/com/guang/app/model/Score.java
@@ -1,14 +1,30 @@
 package com.guang.app.model;
 
 /**
+ * 成绩
  * Created by xiaoguang on 2017/2/14.
  */
 public class Score {
+
+    /**
+     * time : 2014-2015-1
+     * name : 现代空间信息技术导论
+     * score : 77
+     * credit : 1
+     * classCode : 160381
+     * dailyScore : 85
+     * expScore : 0
+     * paperScore : 74
+     */
 
     private String time;
     private String name;
     private int score;
     private int credit;
+    private String classCode;  //课程编号
+    private int dailyScore;   //平时成绩
+    private int expScore;     //实验成绩
+    private int paperScore;   //卷面期末分
 
     public String getTime() {
         return time;
@@ -17,6 +33,15 @@ public class Score {
     public void setTime(String time) {
         this.time = time;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getScore() {
         return score;
     }
@@ -33,12 +58,35 @@ public class Score {
         this.credit = credit;
     }
 
-    public String getName() {
-        return name;
+    public String getClassCode() {
+        return classCode;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setClassCode(String classCode) {
+        this.classCode = classCode;
     }
 
+    public int getDailyScore() {
+        return dailyScore;
+    }
+
+    public void setDailyScore(int dailyScore) {
+        this.dailyScore = dailyScore;
+    }
+
+    public int getExpScore() {
+        return expScore;
+    }
+
+    public void setExpScore(int expScore) {
+        this.expScore = expScore;
+    }
+
+    public int getPaperScore() {
+        return paperScore;
+    }
+
+    public void setPaperScore(int paperScore) {
+        this.paperScore = paperScore;
+    }
 }
